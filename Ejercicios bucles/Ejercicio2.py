a96c5385688e66669a5f9cad5dc418a3146f52dc
CJ\mmardel <pintalac@gmail.com>
2023-11-06 12:44:03 +0100

    excepciones

--- Ejercicios bucles/Ejercicio2.py
+++ Ejercicios bucles/Ejercicio2.py
@@ -1,9 +1,19 @@
 #EJERCICIO 2:
 #Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los 
 #años que ha cumplido (desde 1 hasta su edad).
-edad=int(input("Edad actual: "))
 
-i=1
-while i <= edad:
-    print (i)
-    i+=1
+ 
+while True:
+
+    try:
+        edad=int(input("Edad actual: "))
+    
+        i=1
+        while i <= edad:
+            print (i)
+            i+=1    
+        break
+    except ValueError:
+        print("Vuelve a intentarlo")
+        continue
+        
--- Ejercicios bucles/Ejercicio3.py
+++ Ejercicios bucles/Ejercicio3.py
@@ -2,16 +2,25 @@
 #Escribir un programa que pida al usuario un n√∫mero entero positivo y muestre por 
 #pantalla todos los n√∫meros impares desde 1 hasta ese n√∫mero separados por comas.
 
-numero = int(input("Ingrese un número entero positivo: "))
+while True:
+    try: 
+        numero = int(input("Ingrese un número entero positivo: "))
+        
+        if numero <= 0:
+            print("Por favor, ingrese un número entero positivo.")
+            continue
+        else:
+            impares = ""
+            for i in range(1, numero + 1):
+                if i % 2 != 0:  
+                    if impares:
+                        impares += ", "
+                    impares += str(i)
+            break        
+        print("Números impares desde 1 hasta", numero, ":", impares)
+    except ValueError:
+        print("Introduce un numero entero positivo")
+        continue
+    
+    
 
-if numero <= 0:
-    print("Por favor, ingrese un número entero positivo.")
-else:
-    impares = ""
-    for i in range(1, numero + 1):
-        if i % 2 != 0:  
-            if impares:
-                impares += ", "
-            impares += str(i)
-
-    print("Números impares desde 1 hasta", numero, ":", impares)
