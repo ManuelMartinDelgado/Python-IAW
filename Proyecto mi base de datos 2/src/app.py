afa92803a50327e65fde101d3cd79d29da284126
PCLUCKY\Bylucky <pintalac@gmail.com>
2023-12-09 18:28:19 +0100

    final del proyecto

--- Proyecto mi base de datos 2/src/app.py
+++ Proyecto mi base de datos 2/src/app.py
@@ -10,12 +10,12 @@ app = Flask(__name__, template_folder = template_dir)
 app.secret_key = secrets.token_hex(16)
 
 #Rutas de la aplicación
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('registro.html')
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     email=request.form['email']
     contraseña=request.form['contraseña']
@@ -30,16 +30,16 @@ def login():
         session['nombre']= user[1]
         session ['apellido']= user[2] 
         
-        return redirect('crud')
+        return redirect(url_for('crud'))
     else:
         return render_template('login.html', message="Las credenciales no son correctas")   
 
 #Cerrar sesión con el usuario conectado.
-@app.route('/logout', methods=['GET'])
+@app.route('/logout', methods=['POST', 'GET'])
 def logout():
     #Elimina todas las variables de sesión que estén generadas.
     session.clear()
-    return redirect(url_for('home'))
+    return render_template('login.html')
 
 #Registrar un usuario en la base de datos.
 @app.route('/registro', methods=['POST'])
@@ -48,17 +48,17 @@ def registro():
     apellido= request.form['apellido']
     email = request.form['email']
     contraseña = request.form['contraseña']
-    confirmar = request.form['confirmar']
+    confirmar = request.form['contraseña']
 
     if  nombre and apellido and email and contraseña and confirmar :
         cursor = db.database.cursor()
-        sql = "INSERT INTO registro (nombre, apellido, email, contraseña, confirmar) VALUES (%s,%s,%s, %s, %s)"
+        sql = "INSERT INTO registro (nombre, apellido, email, contraseña, confirmar) VALUES (%s, %s, %s, %s, %s)"
         data = (nombre, apellido, email, contraseña, confirmar)
         cursor.execute(sql, data)
         db.database.commit()
-    return redirect(url_for('crud'))
+    return render_template('login.html')
 
-
+#Formulario
 @app.route('/crud', methods=['GET'])
 def crud():
     cursor = db.database.cursor()
@@ -74,22 +74,25 @@ def crud():
 
 
 
-#Ruta para guardar usuarios en la bdd
+#Ruta para guardar datos de usuarios en la bdd
 @app.route('/user', methods=['POST'])
 def addUser():
-    nombre = request.form['nombre']
-    usuario = request.form['usuario']
-    coche = request.form['coche']
-    rango = request.form['rango']
+    try:
+        nick = request.form['nick']
+        coche = request.form['coche']
+        elo = request.form['elo']
+        partidos = int(request.form['partidos'])
+        region = request.form['region']
 
-    if  nombre and usuario and coche and rango :
-        cursor = db.database.cursor()
-        sql = "INSERT INTO info (nombre,usuario, coche, rango) VALUES (%s, %s, %s, %s)"
-        data = (nombre, usuario, coche, rango)
-        cursor.execute(sql, data)
-        db.database.commit()
-    return redirect(url_for('crud'))
-
+        if  nick and coche and elo and partidos and region:
+            cursor = db.database.cursor()
+            sql = "INSERT INTO info (nick, coche, elo, partidos, region) VALUES (%s, %s, %s, %s, %s)"
+            data = (nick, coche, elo, partidos, region)
+            cursor.execute(sql, data)
+            db.database.commit()
+        return redirect(url_for('crud'))
+    except ValueError:
+        return render_template('index.html', message='Error: El valor de "partidos" debe ser numerico')
 #Borrar los usuarios
 @app.route('/delete/<string:id>')
 def delete(id):
@@ -103,15 +106,17 @@ def delete(id):
 #Editar los datos del formulario
 @app.route('/edit/<string:id>', methods=['POST'])
 def edit(id):
-    nombre = request.form['nombre']
-    usuario = request.form['usuario']
+    
+    nick = request.form['nick']
     coche = request.form['coche']
-    rango = request.form['rango']
+    elo = request.form['elo']
+    partidos = request.form['partidos']
+    region = request.form['region']
 
-    if nombre and usuario and coche and rango:
+    if nick and coche and elo and partidos and region:
         cursor = db.database.cursor()
-        sql = "UPDATE info SET nombre = %s, usuario = %s, coche = %s, rango = %s WHERE id = %s"
-        data = (nombre, usuario, coche, rango, id)
+        sql = "UPDATE info SET nick = %s, coche = %s, elo = %s, partidos = %s, region = %s WHERE id = %s"
+        data = (nick, coche, elo, partidos, region, id)
         cursor.execute(sql, data)
         db.database.commit()
     return redirect(url_for('crud'))
