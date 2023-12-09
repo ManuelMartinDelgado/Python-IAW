f51634b91710445bbfdd868bd865ad0c013e8ceb
PCLUCKY\Bylucky <pintalac@gmail.com>
2023-12-09 02:26:43 +0100

    commit parte funcional terminada

--- Proyecto mi base de datos 2/src/app.py
+++ Proyecto mi base de datos 2/src/app.py
@@ -1,15 +1,66 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import os
 import database as db
+import secrets
 
 template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 template_dir = os.path.join(template_dir, 'src', 'templates')
 
 app = Flask(__name__, template_folder = template_dir)
+app.secret_key = secrets.token_hex(16)
 
 #Rutas de la aplicación
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
+    return render_template('registro.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    email=request.form['email']
+    contraseña=request.form['contraseña']
+    cur=db.database.cursor()
+    cur.execute('SELECT * FROM registro WHERE email= %s AND contraseña = %s', (email,contraseña))
+    user= cur.fetchone()
+    cur.close()
+    
+    if user is not None:
+        session['email'] = email
+        session['contraseña'] = contraseña
+        session['nombre']= user[1]
+        session ['apellido']= user[2] 
+        
+        return redirect('crud')
+    else:
+        return render_template('login.html', message="Las credenciales no son correctas")   
+
+#Cerrar sesión con el usuario conectado.
+@app.route('/logout', methods=['GET'])
+def logout():
+    #Elimina todas las variables de sesión que estén generadas.
+    session.clear()
+    return redirect(url_for('home'))
+
+#Registrar un usuario en la base de datos.
+@app.route('/registro', methods=['POST'])
+def registro():
+    nombre= request.form['nombre']
+    apellido= request.form['apellido']
+    email = request.form['email']
+    contraseña = request.form['contraseña']
+    confirmar = request.form['confirmar']
+
+    if  nombre and apellido and email and contraseña and confirmar :
+        cursor = db.database.cursor()
+        sql = "INSERT INTO registro (nombre, apellido, email, contraseña, confirmar) VALUES (%s,%s,%s, %s, %s)"
+        data = (nombre, apellido, email, contraseña, confirmar)
+        cursor.execute(sql, data)
+        db.database.commit()
+    return redirect(url_for('crud'))
+
+
+@app.route('/crud', methods=['GET'])
+def crud():
     cursor = db.database.cursor()
     cursor.execute("SELECT * FROM info")
     myresult = cursor.fetchall()
@@ -37,8 +88,9 @@ def addUser():
         data = (nombre, usuario, coche, rango)
         cursor.execute(sql, data)
         db.database.commit()
-    return redirect(url_for('home'))
+    return redirect(url_for('crud'))
 
+#Borrar los usuarios
 @app.route('/delete/<string:id>')
 def delete(id):
     cursor = db.database.cursor()
@@ -46,8 +98,9 @@ def delete(id):
     data = (id,)
     cursor.execute(sql, data)
     db.database.commit()
-    return redirect(url_for('home'))
+    return redirect(url_for('crud'))
 
+#Editar los datos del formulario
 @app.route('/edit/<string:id>', methods=['POST'])
 def edit(id):
     nombre = request.form['nombre']
@@ -61,7 +114,7 @@ def edit(id):
         data = (nombre, usuario, coche, rango, id)
         cursor.execute(sql, data)
         db.database.commit()
-    return redirect(url_for('home'))
+    return redirect(url_for('crud'))
 
 
     
